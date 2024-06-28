43d5c7c21980b4c931a6ea88d9b9d5d43a4e219e
t_iwaki <t_iwaki@192.168.227.129>
2024-06-28 11:16:30 +0900

    iwa

--- src/dao/StudentDAO.java
+++ src/dao/StudentDAO.java
@@ -9,32 +9,6 @@ import java.util.List;
 import bean.Student;
 
 public class StudentDAO extends DAO {
-    public List<Student> search(String keyword) throws Exception {
-        List<Student> list = new ArrayList<>();
-        Connection con = getConnection();
-
-        PreparedStatement st = con.prepareStatement(
-            "SELECT * FROM student WHERE name LIKE ?");
-        st.setString(1, "%" + keyword + "%");
-        ResultSet rs = st.executeQuery();
-
-        while (rs.next()) {
-            Student s = new Student();
-            s.setNo(rs.getInt("no"));
-            s.setName(rs.getString("name"));
-            s.setEntYear(rs.getInt("entyear"));
-            s.setClassNum(rs.getString("classnum"));
-            s.setAttend(rs.getBoolean("isattend"));
-            s.setSchoolCd(rs.getString("schoolcd"));
-            list.add(s);
-        }
-
-        rs.close();
-        st.close();
-        con.close();
-
-        return list;
-    }
 
 
     public List<Student> filter(int entYear, String classNum, boolean isAttend) throws Exception {
---
+++ src/dao/SubjectDAO.java
@@ -0,0 +1,37 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.Subject;
+
+public class SubjectDAO extends DAO {
+
+
+    public List<Subject> filter(String school_cd, String cd, String name) throws Exception {
+        List<Subject> list = new ArrayList<>();
+        Connection con = getConnection();
+
+        String query = "SELECT * FROM SUBJECT WHERE SCHOOL_CD= ?";
+        PreparedStatement st = con.prepareStatement(query);
+        st.setString(1, school_cd);
+        ResultSet rs = st.executeQuery();
+
+        while (rs.next()) {
+            Subject s = new Subject();
+            s.setCd(rs.getString("cd"));
+            s.setName(rs.getString("name"));
+            s.setSubjectcd(rs.getString("school_cd"));
+            list.add(s);
+        }
+
+        rs.close();
+        st.close();
+        con.close();
+
+        return list;
+    }
+}
